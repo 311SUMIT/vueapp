f8d98e43a3cee633d7463219190147c740688bef
311SUMIT <38354283+311SUMIT@users.noreply.github.com>
2020-09-09 12:41:36 +0530

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,29 @@
+0.<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <input type="text" v-model="message">
+    <p>{{message}}</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['msg'],
+  data(){
+    return{
+      message:''
+    }
+  }
+ 
+}
+  
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
